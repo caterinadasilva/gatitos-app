d4694028d186c16192c1e8774e2d8d1c3e3c8d4d
Syrimande <caterinadasilva@gmail.com>
2017-07-07 09:07:07 -0400

    fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,3 @@
-FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-});
-
-
-{
-    status: 'connected',
-    authResponse: {
-        accessToken: '...',
-        expiresIn:'...',
-        signedRequest:'...',
-        userID:'...'
-    }
-}
-
-
 function checkLoginState() {
   FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
